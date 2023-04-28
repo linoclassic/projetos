d80e842a5ea23bcefd9001617c41b942a62225ee
Victor Lino <linoxvictorgmail.com>
2023-04-28 16:59:11 -0300

    teste 02

--- script/script.js
+++ script/script.js
@@ -89,3 +89,28 @@ function cancelar() {
 
 
 }
+function gerar(){
+    var xmlHttpRequest = new XMLHttpRequest();
+    var url = "https://dog.ceo/api/breeds/image/random"
+    xmlHttpRequest.open("GET", url, true);
+
+    xmlHttpRequest.onreadystatechange = function() {
+        if (xmlHttpRequest.readyState == 3) {
+            console.log('Carregando o conteúdo');
+        }
+        if (xmlHttpRequest.readyState == 4) {
+             
+            var jsonResponse = JSON.parse(xmlHttpRequest.responseText);
+
+            console.log('Requisição Finalizada');
+            console.log('Resultado da Requisição: ' + jsonResponse);
+            console.log('Resultado da Requisição: ' + jsonResponse.message);
+
+            var imGen = document.getElementById("img_gen");
+            imGen.src = jsonResponse.message;
+        }
+    };
+         
+    xmlHttpRequest.send(null);
+}
+
